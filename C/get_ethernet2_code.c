f636453af25556f8804fd60f6e7268c13b1a91c6
ghost461 <ghost461@localhost.localdomain>
2017-07-18 08:58:23 +0800

    get_ethernet2_code.c complete

--- C/get_ethernet2_code.c
+++ C/get_ethernet2_code.c
@@ -1,3 +1,6 @@
+/*
+ * 连续捕获
+ */
 #include<stdio.h>
 #include<arpa/inet.h>
 #include"pcap.h"
@@ -34,7 +37,7 @@ void ethernet_protocol_packet_callback(u_char *argument,const struct pcap_pkthdr
 	//表示捕获数据包的个数
 	static int packet_number = 1;
 
-	printf("********************************************\n");
+	printf("***************************************************\n");
 	printf("The %d Ethernet packet is captured.\n",packet_number);
 	printf("---------- Ethernet Potocol (Link Layer) ----------\n");
 	printf("The %d Ethernet packet is captured.\n",packet_number);
@@ -65,7 +68,7 @@ void ethernet_protocol_packet_callback(u_char *argument,const struct pcap_pkthdr
 	mac_string = ethernet_protocol->ether_dhost;
 	printf("%02x:%02x:%02x:%02x:%02x:%02x\n", *mac_string , *(mac_string + 1) ,*(mac_string + 2) ,*(mac_string + 3) ,*(mac_string + 4) ,*(mac_string + 5));
 
-	printf("********************************************\n");
+	printf("***************************************************\n");
 	packet_number++;
 }
 int main()
@@ -90,7 +93,20 @@ int main()
 	//参数列表：网络接口、网络地址、网络掩码、错误信息
 	pcap_lookupnet(net_interface , &net_ip , &net_mask , error_content);
 	//打开网络接口
-	//参数列表：网络接口、数据包大小、混杂模式、等待时间
+	//参数列表：网络接口、数据包大小、混杂模式、等待时间、错误信息
 	pcap_handle = pcap_open_live(net_interface , BUFSIZ , 1 , 0 , error_content);
+	//编译过滤规则
+	//参数列表：Libpcap句柄、BPF过滤规则、过滤规则字符串、优化参数、网络地址
+	pcap_compile(pcap_handle , &bpf_filter , bpf_filter_string , 0 , net_ip);
+	//设置过滤规则
+	//参数列表：Libpcap句柄、BPF过滤规则
+	pcap_setfilter(pcap_handle , &bpf_filter);
+	if(pcap_datalink(pcap_handle) != DLT_EN10MB)
+		return 0;
+	//无限循环捕获网络数据包，注册回到函数 ethernet_protocol_packet_callback(),捕获每个数据包都要调用此回调函数进行操作
+	//参数列表：Libpcap句柄、捕获数据包的个数（此处-1表示无限循环）、回调函数、传递给回调函数的参数
+	pcap_loop(pcap_handle , -1 , ethernet_protocol_packet_callback , NULL);
+	//关闭Libpcap操作
+	pcap_close(pcap_handle);
 	return 0;
 }
